aabcfc0324df4b4a6e9b29e9dd5e166e7f4739ba
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-07-17 22:20:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// Build a Navigation Bar
+// After successful login take user to productlist page automatically
+// Create logout functionality
+// Create Routes
+// Create Shared Layout  : Private Routes and Restricted Routes  DONE
 
-function App() {
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import "./App.css";
+
+//adding paths i would like to my app to go to.
+//home, cart, list, login, products
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Routes>
+        <Route exact path="/" element={<Navigate to={PATHS.PRODUCT_LIST} />} />
+
+        <Route path="/" element={<PrivateRoutes />}>
+          <Route path={PATHS.PRODUCT_LIST} element={<ProductList />} />
+          <Route path={PATHS.PRODUCT_DETAIL} element={<ProductDetail />} />
+          <Route path={PATHS.CART} element={<Cart />} />
+        </Route>
+
+        <Route path="/" element={<RestrictedRoutes />}>
+          <Route path={PATHS.LOGIN} element={<Login />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to={PATHS.PRODUCT_LIST} />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
